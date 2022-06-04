304064f400ffdcd3157e8e4cfc99beade8c7d0a9
Petr Glaser <petr.glaser@dhl.com>
2022-06-04 11:36:37 +0200

    fix(lint): push broken cypress to verify elsewhere

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,13 +1,13 @@
 import { validateEmail } from "./utils"
 
 test("validateEmail returns false for non-emails", () => {
-  expect(validateEmail(undefined)).toBe(false)
-  expect(validateEmail(null)).toBe(false)
-  expect(validateEmail("")).toBe(false)
-  expect(validateEmail("not-an-email")).toBe(false)
-  expect(validateEmail("n@")).toBe(false)
+  ;(expect as any)(validateEmail(undefined)).toBe(false)
+  ;(expect as any)(validateEmail(null)).toBe(false)
+  ;(expect as any)(validateEmail("")).toBe(false)
+  ;(expect as any)(validateEmail("not-an-email")).toBe(false)
+  ;(expect as any)(validateEmail("n@")).toBe(false)
 })
 
 test("validateEmail returns true for emails", () => {
-  expect(validateEmail("kody@example.com")).toBe(true)
+  ;(expect as any)(validateEmail("kody@example.com")).toBe(true)
 })
