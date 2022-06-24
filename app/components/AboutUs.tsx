3bfde9eeecb8f6dbbc252dd4953c633a1114b8a9
Petr Glaser <syreanis+1@gmail.com>
2022-06-24 15:18:28 +0200

    feat(about-us): configurable about-us + improve responsibility

--- app/components/AboutUs.tsx
+++ app/components/AboutUs.tsx
@@ -2,9 +2,17 @@ import { Facebook, Instagram } from "~/icons"
 import { SocialButton } from "./Button"
 import { Typography } from "./Typography"
 
-export function AboutUs() {
+type Props = {
+  readonly people: readonly {
+    readonly name: string
+    readonly position: string
+    readonly email: string
+    readonly image: string
+  }[]
+}
+export function AboutUs({ people }: Props) {
   return (
-    <div className={`flex flex-col items-center justify-center gap-x-36 gap-y-4 px-5 md:flex-row`}>
+    <div className={`flex flex-col items-center justify-center gap-x-36 gap-y-4 px-5 xl:flex-row`}>
       <div className='flex w-fit max-w-xl flex-col'>
         <Typography variant='h2' className='mb-8'>
           O nás
@@ -16,7 +24,7 @@ export function AboutUs() {
         <Typography variant='normal' className='mb-10 max-w-xsProse' component='p'>
           Proto abys mohl být tester, nepotřebuješ mnoho.
         </Typography>
-        <div className='flex flex-row gap-x-10 '>
+        <div className='flex flex-row justify-center gap-x-10 xl:justify-start'>
           <SocialButton to='/'>
             <Facebook width={16} />
           </SocialButton>
@@ -25,55 +33,28 @@ export function AboutUs() {
           </SocialButton>
         </div>
       </div>
-      <div className='flex flex-row gap-x-20'>
-        <div className='flex flex-col gap-y-2'>
-          <img
-            src={"/images/qa_illustration.webp"}
-            width={"204px"}
-            height={"220px"}
-            alt={`Ilustrace ke kurzu`}
-            loading='lazy'
-          />
-          <Typography variant='h3' component='h3'>
-            Petr Glaser
-          </Typography>
-          <Typography variant='normal'>Staff Software Engineer</Typography>
-          <Typography variant='link' className='underline'>
-            info@naucme.it
-          </Typography>
-        </div>
-        <div className='flex flex-col gap-y-2'>
-          <img
-            src={"/images/qa_illustration.webp"}
-            width={"204px"}
-            height={"220px"}
-            alt={`Ilustrace ke kurzu`}
-            loading='lazy'
-          />
-          <Typography variant='h3' component='h3'>
-            Pavel Koudelka
-          </Typography>
-          <Typography variant='normal'>Sales</Typography>
-          <Typography variant='link' className='underline'>
-            info@naucme.it
-          </Typography>
-        </div>
-        <div className='flex flex-col gap-y-2'>
-          <img
-            src={"/images/qa_illustration.webp"}
-            width={"204px"}
-            height={"220px"}
-            alt={`Ilustrace ke kurzu`}
-            loading='lazy'
-          />
-          <Typography variant='h3' component='h3'>
-            Lýdie Hemalová
-          </Typography>
-          <Typography variant='normal'>Software Development Engineer in Test</Typography>
-          <Typography variant='link' className='underline'>
-            info@naucme.it
-          </Typography>
-        </div>
+      <div className='flex flex-col justify-center gap-x-20 md:flex-row'>
+        {people.map((p) => (
+          <div className='flex flex-col gap-y-2' key={p.email}>
+            <img
+              src={p.image}
+              className='w-full'
+              width={"245px"}
+              height={"264px"}
+              alt={`Profilová fotka - ${p.name}`}
+              loading='lazy'
+            />
+            <Typography variant='h3' component='h3' className='text-center'>
+              {p.name}
+            </Typography>
+            <Typography variant='normal' className='text-center'>
+              {p.position}
+            </Typography>
+            <Typography variant='link' className='text-center underline'>
+              {p.email}
+            </Typography>
+          </div>
+        ))}
       </div>
     </div>
   )
--- app/routes/playground.tsx
+++ app/routes/playground.tsx
@@ -10,7 +10,28 @@ import { CatchPoints } from "../components/CatchPoints"
 export default function Playground() {
   return (
     <div className='h-screen bg-background'>
-      <AboutUs />
+      <AboutUs
+        people={[
+          {
+            name: "Petr Glaser",
+            email: "petr.glaser@naucme.it",
+            position: "Staff Software Engineer",
+            image: "/images/qa_illustration.webp",
+          },
+          {
+            name: "Pavel Koudelka",
+            email: "pavel.koudelka@naucme.it",
+            position: "Sales",
+            image: "/images/qa_illustration.webp",
+          },
+          {
+            name: "Lýdie Hemalová",
+            email: "lydie.hemalova@naucme.it",
+            position: "Software Engineer in Test",
+            image: "/images/qa_illustration.webp",
+          },
+        ]}
+      />
       <CourseBox image='/images/qa_illustration.webp' side='left' title='Vývojář' link='/'>
         Práce testera je hledání chyb a upozorňování na ně srozumitelnou formou. Proto abys mohl být tester,
         nepotřebuješ mnoho.
