911ff8abc792715b200b4da8e0d360fa19413e32
Petr Glaser <syreanis+1@gmail.com>
2022-07-12 23:58:52 +0200

    fix(form): fix form responsivity

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -3,9 +3,9 @@ import { DecoratedInput } from "./DecoratedInput"
 
 export function ContactForm() {
   return (
-    <form className='mx-auto flex w-6/12 flex-col gap-y-4'>
+    <form className='mx-auto flex w-10/12 flex-col gap-y-4 md:w-6/12'>
       <DecoratedInput type='text' label='Vaše jméno' placeholder='Zadejte své jméno' required />
-      <div className='flex flex-row gap-9'>
+      <div className='flex flex-col gap-9 md:flex-row'>
         <DecoratedInput type='email' label='Váš e-mail' placeholder='Zadejte svůj email' required />
         <DecoratedInput type='tel' label='Vaše jméno' placeholder='Zadejte svoje číslo' />
       </div>
